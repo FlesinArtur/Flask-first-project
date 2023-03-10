8817bc47c74bdeadc179bff8d500c6b392cbf5ec
artur <flesinartur@gmail.com>
2023-03-10 19:46:52 +0200

    code refactoring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import sqlite3
 
+import flask
 from flask import Flask, request
 from markupsafe import escape
 from flask import render_template
@@ -8,33 +9,30 @@ from flask import render_template
 
 app = Flask(__name__)  # create Flask object
 DATABASE_NAME = "messages.db"
+with sqlite3.connect(DATABASE_NAME) as connection:
+    cur = connection.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS messages(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TEXT,
+    text TEXT
+    )""")
 
 
-def create_table(filename: str):
-    with sqlite3.connect(filename) as connection:
-        cur = connection.cursor()
-        cur.execute("""CREATE TABLE IF NOT EXISTS messages(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        created_at TEXT,
-        text TEXT
-        )""")
-
-
-def create_message(text: str, filename: str):
+def create_message(text: str, filename: str = DATABASE_NAME):
     with sqlite3.connect(filename) as connection:
         cur = connection.cursor()
         created_at = str(datetime.now().strftime("%H:%M"))
-        cur.execute("INSERT INTO messages(created_at, text) VALUES(?,?)", (created_at, text))
-        connection.commit()
+        if text != '':
+            cur.execute("INSERT INTO messages(created_at, text) VALUES(?,?)", (created_at, text))
+            connection.commit()
 
 
-def get_messages(filename: str):
+def get_messages(filename: str = DATABASE_NAME) -> list:
     with sqlite3.connect(filename) as connection:
         cur = connection.cursor()
         table_data = cur.execute("SELECT * FROM messages").fetchall()
         messages = []
-        for tuple_ in table_data:
-            message_id, created_at, text = tuple_
+        for message_id, created_at, text in table_data:
             messages.append({
                 'message_id': message_id,
                 'created_at': created_at,
@@ -60,10 +58,9 @@ def version():
 
 @app.route("/chat/", methods=['GET', 'POST'])
 def chat():
-    create_table(DATABASE_NAME)
-    if request.form.get('message') == '' or request.form.get('message') is None:
-        return render_template("chat.html", messages='')
-    text = request.form.get('message')
-    create_message(text, DATABASE_NAME)
-    messages = get_messages(DATABASE_NAME)
-    return render_template("chat.html", messages=messages)
+    if flask.request.method == 'GET':
+        return render_template("chat.html", messages=get_messages())
+    else:
+        text = request.form.get('message')
+        create_message(text)
+        return render_template("chat.html", messages=get_messages())
