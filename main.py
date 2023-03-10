2dd54a89681b73c129d14176d5429886c1f9c780
artur <flesinartur@gmail.com>
2023-03-10 17:14:06 +0200

    add database instead list

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
+from datetime import datetime
+import sqlite3
+
 from flask import Flask, request
 from markupsafe import escape
 from flask import render_template
-from datetime import datetime
 
 
 app = Flask(__name__)  # create Flask object
-MESSAGES = []
+DATABASE_NAME = "messages.db"
 
 
-def create_message(text):
-    message_id = 0
-    if len(MESSAGES) == 0:
-        message_id = 1
-    else:
-        message_id = MESSAGES[-1]['id'] + 1
-    created_at = str(datetime.now().strftime("%H:%M"))
-    MESSAGES.append({
-        'id': message_id,
-        'created_at': created_at,
-        'text': text
-    })
+def create_table(filename: str):
+    with sqlite3.connect(filename) as connection:
+        cur = connection.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS messages(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        created_at TEXT,
+        text TEXT
+        )""")
+
+
+def create_message(text: str, filename: str):
+    with sqlite3.connect(filename) as connection:
+        cur = connection.cursor()
+        created_at = str(datetime.now().strftime("%H:%M"))
+        cur.execute("INSERT INTO messages(created_at, text) VALUES(?,?)", (created_at, text))
+        connection.commit()
+
+
+def get_messages(filename: str):
+    with sqlite3.connect(filename) as connection:
+        cur = connection.cursor()
+        table_data = cur.execute("SELECT * FROM messages").fetchall()
+        messages = []
+        for tuple_ in table_data:
+            message_id, created_at, text = tuple_
+            messages.append({
+                'message_id': message_id,
+                'created_at': created_at,
+                'text': text
+            })
+    return messages
 
 
 @app.route("/hello")
@@ -39,8 +60,10 @@ def version():
 
 @app.route("/chat/", methods=['GET', 'POST'])
 def chat():
+    create_table(DATABASE_NAME)
     if request.form.get('message') == '' or request.form.get('message') is None:
         return render_template("chat.html", messages='')
     text = request.form.get('message')
-    create_message(text)
-    return render_template("chat.html", messages=MESSAGES)
+    create_message(text, DATABASE_NAME)
+    messages = get_messages(DATABASE_NAME)
+    return render_template("chat.html", messages=messages)
